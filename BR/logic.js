5cdfdbfd5fc16b6d79bda615767b853f8784f9ac
mo7amedehab97 <gazacityface@gmail.com>
2022-02-16 17:20:35 +0200

    add gitignore and dev dependency

--- BR/logic.js
+++ BR/logic.js
@@ -1,19 +1,10 @@
-// search function 
+// search function
 const search = (arr, name) => {
   return arr.filter((ele) => {
     return ele.name.toLowerCase().includes(name.toLowerCase());
   });
 };
-const sortItemsDescending = (arr) => {
-  return arr.sort((a, b) => {
-    return b.char_id - a.char_id;
-  });
-};
-const sortItemsAscending = (arr) => {
-  return arr.sort((a, b) => {
-    return a.char_id - b.char_id;
-  });
-};
+
 if (typeof module !== "undefined") {
-  module.exports = { search, sortItemsDescending, sortItemsAscending };
+  module.exports = { search };
 }
